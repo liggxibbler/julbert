e9495d8ec13c2550e9e20c3f7bafef90d893d582
alb <albizgil@gmail.com>
2021-01-26 00:35:27 +0330

    commit!

---
+++ Assets/Julia.shader
@@ -0,0 +1,75 @@
+﻿Shader "Unlit/Julia"
+{
+    Properties
+    {
+        _Position ("Position", Vector) = (0, 0, 1, 0) // x, y, scalex, scaley
+		_MainTex("Texture", 2D) = "white" {}
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            float4 _Position;
+            
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;// TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+			float2 iterate(float2 p, float2 c)
+			{
+				return float2(p.x * p.x - p.y * p.y + c.x, 2 * p.x * p.y + c.y);
+			}
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				int k;
+				//float2 offset = float2(sin(_Time.y*17 / 50), cos(_Time.y * 19 / 50));
+				float2 offset = float2(_Position.x, _Position.y) * _Position.zw;
+				float2 ret = (i.uv - .5) * 2.0* _Position.zw;
+				ret = ret;
+				float2 c = offset;
+
+				for (k = 0; k < 256; ++k)
+				{
+					ret = iterate(ret, c);
+					if (dot(ret, ret) >= 4.0)
+						break;
+				}
+
+				float j = sqrt(float(k));
+				//fixed4 col = fixed4(sin(j), cos(j), 1/j, 1.0);
+				fixed4 col = 1 - fixed4(1/j, 1/j, 1 / j, 1.0);
+				//float j = 256.0/sqrt(float(k));
+				//fixed4 col = fixed4(j,j,j,1);
+				//col = fixed4(ret.x, ret.y, 0.0, 1.0);
+				return col;
+            }
+            ENDCG
+        }
+    }
+}
---
+++ Assets/MaterialUpdate.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialUpdate : MonoBehaviour
+{
+    public float stepLength = 1;
+    float lastStepTime = 0;
+    float nextStepTime;
+
+    public Material material;
+    public Vector2 position;
+    public float scale;
+    public List<Vector2> points;
+    int sample = 0;
+
+    private void Awake()
+    {
+        nextStepTime = stepLength;
+        points = new List<Vector2>();
+        float scale = 1;
+        var corners = new Vector2[] { new Vector2(-.5f, -.5f) * scale, new Vector2(-.5f, .5f) * scale, new Vector2(.5f, .5f) * scale, new Vector2(.5f, -.5f) * scale };
+        Hilbert(corners, 5, points);
+        Debug.Log(points.Count);
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            scale *= 1.01f;
+        else if (Input.GetKey(KeyCode.DownArrow))
+            scale /= 1.01f;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            stepLength *= 1.01f;
+        else if (Input.GetKey(KeyCode.RightArrow))
+            stepLength /= 1.01f;
+
+        if (Input.GetKeyUp(KeyCode.Escape))
+            Application.Quit();
+
+
+        float aspect = (float)Screen.width / Screen.height;
+        float scalex = scale;
+        float scaley = scale;
+        if (aspect > 1)
+            scalex = scalex * aspect;
+        else
+            scaley = scaley / aspect;
+
+        var diff = Input.mousePosition - .5f * new Vector3(Screen.width, Screen.height, 0);
+        position = new Vector2(diff.x / Screen.width, diff.y/Screen.height);
+
+        //material.SetVector("_Position", new Vector4(position.x, position.y, scalex, scaley));
+        int nextSample = (sample + 1) % points.Count;
+        Vector2 lerp = Vector2.Lerp(points[sample], points[nextSample], (Time.time - lastStepTime) / stepLength);
+        material.SetVector("_Position", new Vector4(lerp.x, lerp.y, scalex, scaley));
+
+        if (Time.time > nextStepTime)
+        {
+            lastStepTime = Time.time;
+            nextStepTime = lastStepTime + stepLength;
+            sample += 1;
+            sample = sample % points.Count;
+        }
+    }
+
+    void Hilbert(Vector2[] corners, int depth, List<Vector2> points)
+    {
+        if (depth == 0)
+        {
+            points.AddRange(corners);
+        }
+        else
+        {
+            Vector2 c = (corners[2] + corners[0]) * .5f;
+
+            var p0 = (corners[0] - c) * .5f;
+            var p1 = (corners[1] - c) * .5f;
+            var p2 = (corners[2] - c) * .5f;
+            var p3 = (corners[3] - c) * .5f;
+
+            Hilbert(new Vector2[] { corners[0] + p0, corners[0] + p3, corners[0] + p2, corners[0] + p1 }, depth - 1, points);
+            Hilbert(new Vector2[] { corners[1] + p0, corners[1] + p1, corners[1] + p2, corners[1] + p3 }, depth - 1, points);
+            Hilbert(new Vector2[] { corners[2] + p0, corners[2] + p1, corners[2] + p2, corners[2] + p3 }, depth - 1, points);
+            Hilbert(new Vector2[] { corners[3] + p2, corners[3] + p1, corners[3] + p0, corners[3] + p3 }, depth - 1, points);
+        }
+    }
+}
